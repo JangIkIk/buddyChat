8ac343536c6ba1a5d928ef11095aefb796d2872d
junik <wkdwnsdlr37@gmail.com>
2025-02-12 21:24:11 +0900

    Style: css 설정 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import App from './app/App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./app/App";
+import "./app/index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
     <App />
-)
+  </StrictMode>
+);
