5504ecb8c1cffb293ffa17432f933c3bb8b5bcd2
junik <wkdwnsdlr37@gmail.com>
2025-02-14 15:18:40 +0900

    Style: 반응형 Header 적용

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,23 +1,37 @@
-import { Modal } from './Modal';
-import { useToggle } from '@/shared/hooks/useToggle';
+import { MenuList } from "./MenuList";
+import { useToggle } from "@/shared/hooks/useToggle";
 import Logo from "@/shared/asset/logo.svg?react";
 import Menu from "@/shared/asset/menu.svg?react";
 
-
 const Header = () => {
-    const [modalToggle, setModalToggle] = useToggle();
+  const [modalToggle, setModalToggle] = useToggle();
 
   return (
-    <div className="@container">
+    <div className="tw:@container tw:text-base">
       <div className="tw:px-4 tw:flex tw:justify-between tw:items-center tw:theme-shade1 tw:h-12.5 tw:md:h-15 tw:lg:h-20 tw:shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
-        <Logo className="tw:w-8 tw:h-8 tw:fill-service-primary tw:cursor-pointer" />
+
+        {/* logo */}
+        <div className="tw:flex tw:items-center tw:gap-3">
+            <Logo className="tw:w-8 tw:h-8 tw:fill-service-primary tw:cursor-pointer tw:@3xl:w-10 tw:@3xl:h-10"/>
+            <span className="tw:italic tw:font-black tw:text-service-primary">HeyBuddyChat</span>
+        </div>
+
+        {/* Mobile Button */}
         <Menu
           onClick={setModalToggle}
-          className="tw:w-6 tw:h-6 tw:fill-service-gray tw:cursor-pointer tw:hover:fill-service-secondary"
+          className="tw:w-6 tw:h-6 tw:fill-service-gray tw:cursor-pointer tw:hover:fill-service-secondary tw:@3xl:hidden"
         />
+
+        {/* Tablet & DeskTop */}
+        <div className="tw:hidden tw:@3xl:flex tw:grow tw:justify-end tw:gap-3">
+            <MenuList/>
+        </div>
+
       </div>
-      <div className="tw:shadow-[0_4px_4px_rgba(0,0,0,0.25)] tw:theme-shade1 tw:absolute tw:left-0 tw:right-0">
-        { modalToggle ? <Modal/> : null}
+
+      {/* Mobile Modal*/}
+      <div className="tw:flex tw:flex-col tw:gap-3 tw:shadow-[0_4px_4px_rgba(0,0,0,0.25)] tw:theme-shade1 tw:absolute tw:left-0 tw:right-0 ">
+        {modalToggle ? <MenuList /> : null}
       </div>
     </div>
   );
