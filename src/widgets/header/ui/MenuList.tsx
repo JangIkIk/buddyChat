b60d3eede1b11557b8b68503fd6d6d49adaf8f10
junik <wkdwnsdlr37@gmail.com>
2025-02-14 17:44:16 +0900

    Style: MenuList css 변경

--- src/widgets/header/ui/MenuList.tsx
+++ src/widgets/header/ui/MenuList.tsx
@@ -6,30 +6,30 @@ import Theme from "@/shared/asset/theme.svg?react";
 export const MenuList = () => {
   return (
     <>
-      <div className="tw:flex tw:gap-3 tw:p-3 tw:group tw:cursor-pointer tw:min-w-30">
+      <div className="tw:flex tw:justify-center tw:gap-3 tw:p-3 tw:group tw:cursor-pointer">
         <Home className="tw:w-5 tw:h-5 tw:text-balck tw:fill-service-gray tw:group-hover:fill-service-secondary" />
-        <span className="tw:group-hover:text-service-secondary tw:text-service-black tw:text-base tw:font-medium">
-          홈
+        <span className="tw:min-w-15 tw:text-center tw:group-hover:text-service-secondary tw:text-service-black tw:text-base tw:font-medium">
+          소개
         </span>
       </div>
 
-      <div className="tw:flex tw:gap-3 tw:p-3 tw:group tw:cursor-pointer tw:min-w-30">
+      <div className="tw:flex tw:justify-center tw:gap-3 tw:p-3 tw:group tw:cursor-pointer">
         <Random className="tw:w-5 tw:h-5 tw:text-balck tw:fill-service-gray tw:group-hover:fill-service-secondary" />
-        <span className="tw:group-hover:text-service-secondary tw:text-service-black tw:text-base tw:font-medium">
+        <span className="tw:min-w-15 tw:text-center tw:group-hover:text-service-secondary tw:text-service-black tw:text-base tw:font-medium">
           랜덤채팅
         </span>
       </div>
 
-      <div className="tw:flex tw:gap-3 tw:p-3 tw:group tw:cursor-pointer tw:min-w-30">
+      <div className="tw:flex tw:justify-center tw:gap-3 tw:p-3 tw:group tw:cursor-pointer">
         <Group className="tw:w-5 tw:h-5 tw:text-balck tw:fill-service-gray tw:group-hover:fill-service-secondary" />
-        <span className="tw:group-hover:text-service-secondary tw:text-service-black tw:text-base tw:font-medium">
+        <span className="tw:min-w-15 tw:text-center tw:group-hover:text-service-secondary tw:text-service-black tw:text-base tw:font-medium">
           그룹채팅
         </span>
       </div>
 
-      <div className="tw:flex tw:gap-3 tw:p-3 tw:group tw:cursor-pointer tw:min-w-30">
+      <div className="tw:flex tw:justify-center tw:gap-3 tw:p-3 tw:group tw:cursor-pointer">
         <Theme className="tw:w-5 tw:h-5 tw:text-balck tw:fill-service-gray tw:group-hover:fill-service-secondary" />
-        <span className="tw:group-hover:text-service-secondary tw:text-service-black tw:text-base tw:font-medium">
+        <span className="tw:min-w-15 tw:text-center tw:group-hover:text-service-secondary tw:text-service-black tw:text-base tw:font-medium">
           테마변경
         </span>
       </div>
