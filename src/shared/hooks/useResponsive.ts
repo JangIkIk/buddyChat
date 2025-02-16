878b12f7fb808480003df534de16efb78342e9c8
junik <wkdwnsdlr37@gmail.com>
2025-02-16 19:10:29 +0900

    Feat: useResponsive 생성

---
+++ src/shared/hooks/useResponsive.ts
@@ -0,0 +1,19 @@
+import { useMediaQuery } from "react-responsive";
+
+export const useResponsive = ( props: PropsType ): boolean => {
+    const { device, callback } = props;
+    
+    const width = {
+        mobile: {maxWidth: 767},
+        tablet: {minWidth: 768},
+        desktop: {minWidth: 1024}
+    }
+    const isDevice = useMediaQuery( width[device], undefined, (state) => {callback(state)});
+    
+    return isDevice;
+};
+
+type PropsType = {
+    device: "mobile" | "tablet" | "desktop";
+    callback: ( state: boolean ) => void;
+} & {};
