759aeaad8cbc657186a4fddb555001294e6b95ea
junik <wkdwnsdlr37@gmail.com>
2025-02-19 08:20:35 +0900

    Style: Header 임시 테마변경적용

--- src/widgets/header/ui/MenuList.tsx
+++ src/widgets/header/ui/MenuList.tsx
@@ -5,32 +5,37 @@ import Group from "@/shared/asset/group.svg?react";
 import Theme from "@/shared/asset/theme.svg?react";
 
 export const MenuList = () => {
+
+  const toggleTheme = () => {
+    document.documentElement.classList.toggle("dark");
+  }
+
   return (
     <>
       <Link to="/" className="tw:flex tw:justify-center tw:items-center tw:p-3 tw:gap-3 tw:group tw:cursor-pointer">
         <Home className="tw:w-5 tw:h-5 tw:text-balck tw:fill-service-gray tw:group-hover:fill-service-secondary" />
-        <span className="tw:min-w-15 tw:text-center tw:group-hover:text-service-secondary tw:text-service-black tw:text-base tw:font-medium">
+        <span className="tw:min-w-15 tw:text-center tw:group-hover:text-service-secondary tw:c-text-theme-base tw:text-base tw:font-medium">
           소개
         </span>
       </Link>
 
       <Link to="/random" className="tw:flex tw:justify-center tw:gap-3 tw:p-3 tw:group tw:cursor-pointer">
         <Random className="tw:w-5 tw:h-5 tw:text-balck tw:fill-service-gray tw:group-hover:fill-service-secondary" />
-        <span className="tw:min-w-15 tw:text-center tw:group-hover:text-service-secondary tw:text-service-black tw:text-base tw:font-medium">
+        <span className="tw:min-w-15 tw:text-center tw:group-hover:text-service-secondary tw:c-text-theme-base tw:text-base tw:font-medium">
           랜덤채팅
         </span>
       </Link>
 
       <Link to="/group" className="tw:flex tw:justify-center tw:gap-3 tw:p-3 tw:group tw:cursor-pointer">
         <Group className="tw:w-5 tw:h-5 tw:text-balck tw:fill-service-gray tw:group-hover:fill-service-secondary" />
-        <span className="tw:min-w-15 tw:text-center tw:group-hover:text-service-secondary tw:text-service-black tw:text-base tw:font-medium">
+        <span className="tw:min-w-15 tw:text-center tw:group-hover:text-service-secondary tw:c-text-theme-base tw:text-base tw:font-medium">
           그룹채팅
         </span>
       </Link>
 
-      <button className="tw:flex tw:justify-center tw:gap-3 tw:p-3 tw:group tw:cursor-pointer">
+      <button className="tw:flex tw:justify-center tw:gap-3 tw:p-3 tw:group tw:cursor-pointer" onClick={toggleTheme}>
         <Theme className="tw:w-5 tw:h-5 tw:text-balck tw:fill-service-gray tw:group-hover:fill-service-secondary" />
-        <span className="tw:min-w-15 tw:text-center tw:group-hover:text-service-secondary tw:text-service-black tw:text-base tw:font-medium">
+        <span className="tw:min-w-15 tw:text-center tw:group-hover:text-service-secondary tw:c-text-theme-base tw:text-base tw:font-medium">
           테마변경
         </span>
       </button>
