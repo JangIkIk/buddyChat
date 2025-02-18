fc06790be2bc5c75dbeafdd1d87356931f8a2d7d
junik <wkdwnsdlr37@gmail.com>
2025-02-18 15:15:39 +0900

    Feat: useResponsive 모바일 상태변경 적용

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -3,44 +3,65 @@ import { useToggle } from "@/shared/hooks/useToggle";
 import Logo from "@/shared/asset/logo.svg?react";
 import Menu from "@/shared/asset/menu.svg?react";
 import { useResponsive } from "@/shared/hooks/useResponsive";
-
+import { cn } from "@/shared/lib/cn";
 
 const Header = () => {
   const [modalToggle, setModalToggle, optionalToggle] = useToggle();
-  const isMobile = useResponsive({device: 'mobile', callback: ( state ) => {
-    if(modalToggle && !state){
+
+  const isMobile = useResponsive({
+    device: "mobile",
+    callback: (state) => {
+      if (modalToggle && !state) {
         optionalToggle(false);
-    }
-  }})
+      }
+    },
+  });
 
   return (
-    <div className="tw:@container tw:text-base tw:theme-shade1 tw:shadow-[0_4px_4px_rgba(0,0,0,0.25)] tw:h-12.5 tw:md:h-15 tw:lg:h-20">
-      {/* wrap */}
-      <div className="tw:h-full tw:px-4 tw:flex tw:justify-between tw:items-center tw:relative">
+    <header className="tw:@container tw:text-shadow-lg tw:c-shadow-bt tw:fixed tw:w-full tw:h-15 tw:c-bg-theme-strong">
+      <div className="tw:max-w-320 tw:h-full tw:m-auto tw:px-4 tw:flex tw:justify-between tw:items-center">
         {/* logo */}
-        <div className="tw:flex tw:items-center tw:gap-3">
-          <Logo className="tw:w-8 tw:h-8 tw:fill-service-primary tw:cursor-pointer tw:@3xl:w-10 tw:@3xl:h-10" />
-          <span className="tw:italic tw:font-black tw:text-service-primary">
-            HeyBuddyChat
+        <div className="tw:flex tw:items-center tw:gap-1">
+          <Logo className="tw:w-8 tw:h-8 tw:fill-service-primary tw:cursor-pointer"/>
+          <span
+            className={cn(
+              "tw:text-service-primary tw:text-base tw:font-bold",
+              "tw:@3xl:text-lg tw:@5xl:text-xl"
+            )}
+          >
+            BuddyChat
           </span>
         </div>
 
-        {/* Mobile */}
-        { isMobile ? <> <Menu
-          onClick={setModalToggle}
-          className={`tw:w-6 tw:h-6 ${modalToggle ? "tw:fill-service-secondary" : "tw:fill-service-gray"} tw:cursor-pointer tw:hover:fill-service-secondary tw:@3xl:hidden`}
-        />
-        <div className="tw:flex tw:flex-col tw:gap-3 tw:shadow-[0_4px_4px_rgba(0,0,0,0.25)] tw:theme-shade1 tw:absolute tw:left-0 tw:right-0 tw:bottom-0 tw:translate-y-full">
-          {modalToggle ? <MenuList /> : null}
-        </div></> : null}
-       
-
-        {/* Tablet & DeskTop */}
-        <div className="tw:hidden tw:@3xl:flex tw:grow tw:justify-end tw:gap-3">
-          <MenuList />
-        </div>
+        {isMobile ? (
+          // Mobile
+          <>
+            <Menu
+              onClick={setModalToggle}
+              className={cn(
+                "tw:w-5 tw:h-5 tw:cursor-pointer tw:fill-service-gray tw:hover:fill-service-secondary",
+                { "tw:fill-service-secondary": modalToggle }
+              )}
+            />
+            <div
+              className={cn(
+                "tw:hidden tw:flex-col tw:gap-3 tw:shadow-down tw:theme-shade1 tw:absolute tw:left-0 tw:right-0 tw:bottom-0 tw:translate-y-full",
+                {
+                  "tw:flex": modalToggle,
+                }
+              )}
+            >
+              <MenuList />
+            </div>
+          </>
+        ) : (
+          // Tablet & DeskTop
+          <div className="tw:flex tw:grow tw:justify-end tw:gap-3">
+            <MenuList />
+          </div>
+        )}
       </div>
-    </div>
+    </header>
   );
 };
 
