e0fff7fa908cc6c8e4f8c21a1b150837f787406d
junik <wkdwnsdlr37@gmail.com>
2025-02-18 20:21:39 +0900

    Refactor: MenuList Link 태그적용

--- src/widgets/header/ui/MenuList.tsx
+++ src/widgets/header/ui/MenuList.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Home from "@/shared/asset/home.svg?react";
 import Random from "@/shared/asset/random.svg?react";
 import Group from "@/shared/asset/group.svg?react";
@@ -6,33 +7,33 @@ import Theme from "@/shared/asset/theme.svg?react";
 export const MenuList = () => {
   return (
     <>
-      <div className="tw:flex tw:justify-center tw:gap-3 tw:p-3 tw:group tw:cursor-pointer">
+      <Link to="/" className="tw:flex tw:justify-center tw:items-center tw:p-3 tw:gap-3 tw:group tw:cursor-pointer">
         <Home className="tw:w-5 tw:h-5 tw:text-balck tw:fill-service-gray tw:group-hover:fill-service-secondary" />
         <span className="tw:min-w-15 tw:text-center tw:group-hover:text-service-secondary tw:text-service-black tw:text-base tw:font-medium">
           소개
         </span>
-      </div>
+      </Link>
 
-      <div className="tw:flex tw:justify-center tw:gap-3 tw:p-3 tw:group tw:cursor-pointer">
+      <Link to="/random" className="tw:flex tw:justify-center tw:gap-3 tw:p-3 tw:group tw:cursor-pointer">
         <Random className="tw:w-5 tw:h-5 tw:text-balck tw:fill-service-gray tw:group-hover:fill-service-secondary" />
         <span className="tw:min-w-15 tw:text-center tw:group-hover:text-service-secondary tw:text-service-black tw:text-base tw:font-medium">
           랜덤채팅
         </span>
-      </div>
+      </Link>
 
-      <div className="tw:flex tw:justify-center tw:gap-3 tw:p-3 tw:group tw:cursor-pointer">
+      <Link to="/group" className="tw:flex tw:justify-center tw:gap-3 tw:p-3 tw:group tw:cursor-pointer">
         <Group className="tw:w-5 tw:h-5 tw:text-balck tw:fill-service-gray tw:group-hover:fill-service-secondary" />
         <span className="tw:min-w-15 tw:text-center tw:group-hover:text-service-secondary tw:text-service-black tw:text-base tw:font-medium">
           그룹채팅
         </span>
-      </div>
+      </Link>
 
-      <div className="tw:flex tw:justify-center tw:gap-3 tw:p-3 tw:group tw:cursor-pointer">
+      <button className="tw:flex tw:justify-center tw:gap-3 tw:p-3 tw:group tw:cursor-pointer">
         <Theme className="tw:w-5 tw:h-5 tw:text-balck tw:fill-service-gray tw:group-hover:fill-service-secondary" />
         <span className="tw:min-w-15 tw:text-center tw:group-hover:text-service-secondary tw:text-service-black tw:text-base tw:font-medium">
           테마변경
         </span>
-      </div>
+      </button>
     </>
   );
 };
