471ff27c27ea5336ba7f36d0bee59aab36b999e0
Jangmy <108041161+JangIkIk@users.noreply.github.com>
2025-02-22 14:28:47 +0900

    Home page (#3)
    * Style: Header 임시 테마변경적용
    
    * Chore: tsconfig 구성 재설정
    
    * Style: baseLayout 생성
    
    * Style: baseLayout css변경
    
    * Style: c-linear-base 클래스 색상변경
    
    * Style: Home 기본 디자인 생성
    
    * Style: header-h-base 변수생성 및 적용

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { BaseLayout } from './layout/baseLayout';
+import { BaseLayout } from './layout/BaseLayout';
+import { Home } from "@/pages/home";
 
 function App() {
 
@@ -7,7 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<BaseLayout/>}>
-        <Route index element={<div>홈</div>}/>
+        <Route index element={<Home/>}/>
         <Route path="/random" element={<div>랜덤</div>}/>
         <Route path="/group" element={<div>그룹</div>}/>
         <Route path="/info" element={<div>정보</div>}/>
--- src/app/layout/BaseLayout.tsx
+++ src/app/layout/BaseLayout.tsx
@@ -1,13 +1,15 @@
-import { Header } from '@/widgets/header';
-import { Outlet } from 'react-router-dom';
+import { Header } from "@/widgets/header";
+import { Outlet } from "react-router-dom";
 
 export const BaseLayout = () => {
-    return (
-        <>
-            <Header/>
-            <main className='tw:pt-15'>
-            <Outlet/>
-            </main>
-        </>
-    );
-}
+  return (
+    <div className="tw:flex tw:flex-col">
+      <header className="tw:h-(--header-h-base)">
+        <Header />
+      </header>
+       <main className="tw:grow tw:h-[calc(100vh-var(--header-h-base))]">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
