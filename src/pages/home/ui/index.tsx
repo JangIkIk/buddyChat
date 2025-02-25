3318c6d11cb92df196ca21fd79fd70427f9a460e
junik <wkdwnsdlr37@gmail.com>
2025-02-25 21:10:15 +0900

    Style: 반응형 css적용

--- src/pages/home/ui/index.tsx
+++ src/pages/home/ui/index.tsx
@@ -2,6 +2,7 @@ import { text } from "../text";
 import { cn } from "@/shared/lib/cn";
 
 const Home = () => {
+  
   return (
     <div
       className={cn(
@@ -27,12 +28,12 @@ const Home = () => {
         </button>
       </section>
 
-      {/* section2 */}
+      {/* section2 = 백엔드 데이터 연결*/}
       <section className="tw:snap-always tw:p-4 tw:snap-start tw:h-full tw:flex tw:flex-col tw:gap-5 tw:max-w-[1024px] tw:m-auto tw:overflow-y-scroll">
         <h2 className="tw:font-bold tw:text-xl">이런게 가능해요!</h2>
-        {text.chatInfo.map((item) => {
+        {text.chatInfo.map((item, idx) => {
           return (
-            <div>
+            <div key={idx}>
               <span className="tw:font-bold">{item.title}</span>
               <p className="tw:text-service-gray tw:shadow-md tw:p-4 tw:mt-2 tw:rounded-b-lg">
                 {item.description}
@@ -42,24 +43,32 @@ const Home = () => {
         })}
       </section>
 
-      {/* section3 */}
-      <section className="tw:snap-always tw:p-4 tw:snap-start tw:h-full tw:px-4 tw:max-w-[1024px] tw:m-auto">
+      {/* section3 = 백엔드 데이터 연결 */}
+      <section
+        className="tw:snap-always tw:p-4 tw:snap-start tw:h-full tw:px-4 tw:max-w-[1024px] tw:m-auto"
+      >
         <h2 className="tw:font-bold tw:text-xl">새로운소식</h2>
         <ul className="tw:mt-2 tw:flex tw:flex-col tw:gap-5">
-          <li className="tw:p-4 tw:flex tw:flex-col tw:gap-4 tw:c-shadow-bt tw:rounded-lg tw:c-bg-theme-strong tw:hover:bg-service-secondary tw:group tw:cursor-pointer">
+          <li className="tw:p-4 tw:flex tw:flex-col tw:gap-4 tw:c-shadow-bt tw:rounded-lg tw:c-bg-theme-strong">
             <div className="tw:flex tw:items-center tw:justify-between">
-              <span className="tw:font-bold tw:group-hover:text-service-white">
+              <span className="tw:font-bold ">
                 2025-03-01
               </span>
-              <span className={cn("tw:text-sm tw:text-service-gray tw:group-hover:text-service-white",
-                "tw:md:text-base"
-              )}>
+              <span
+                className={cn(
+                  "tw:text-sm tw:text-service-gray ",
+                  "tw:md:text-base"
+                )}
+              >
                 v1.0.0
               </span>
             </div>
-            <p className="tw:group-hover:text-service-white">
-              랜덤채팅 & 그룹채팅이 가능해요!
-            </p>
+            <details className="tw:cursor-pointer tw:duration-[0.5s] tw:transition-[max-height] tw:max-h-10 tw:open:max-h-100 tw:overflow-hidden">
+              <summary className="tw:hover:text-service-primary tw:marker:text-service-primary">랜덤채팅 & 그룹채팅이 가능해요!</summary>
+              <p className="tw:overflow-y-scroll tw:max-h-100 tw:p-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque animi porro cum minus optio consectetur ducimus, nesciunt, aliquid perspiciatis ipsam in libero. Enim delectus quidem veniam temporibus dolore voluptas nam.
+              </p>
+            </details>
           </li>
         </ul>
       </section>
