4ee7188389b7ce46cac2d46ef1325923ec77c61e
junik <wkdwnsdlr37@gmail.com>
2025-02-28 11:47:55 +0900

    Feat: modal Effect 설정

--- src/widgets/header/ui/index.tsx
+++ src/widgets/header/ui/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { MenuList } from "./MenuList";
 import { useToggle } from "@/shared/hooks/useToggle";
 import Logo from "@/shared/asset/logo.svg?react";
@@ -7,6 +9,7 @@ import { cn } from "@/shared/lib/cn";
 
 const Header = () => {
   const [modalToggle, setModalToggle, optionalToggle] = useToggle();
+  const location = useLocation();
 
   const isMobile = useResponsive({
     device: "mobile",
@@ -17,12 +20,17 @@ const Header = () => {
     },
   });
 
+  useEffect(()=>{
+    setModalToggle();
+  },[location.pathname])
+
   return (
     <div className="tw:@container tw:text-shadow-lg tw:c-shadow-bt tw:fixed tw:w-full tw:h-(--header-h-base) tw:c-bg-theme-strong">
       <div className="tw:max-w-320 tw:h-full tw:m-auto tw:px-4 tw:flex tw:justify-between tw:items-center">
         {/* logo */}
+        <Link to={'/'}>
         <div className="tw:flex tw:items-center tw:gap-1">
-          <Logo className="tw:w-8 tw:h-8 tw:fill-service-primary tw:cursor-pointer" />
+          <Logo className="tw:w-8 tw:h-8 tw:fill-service-primary" />
           <span
             className={cn(
               "tw:text-service-primary tw:text-base tw:font-bold",
@@ -32,6 +40,7 @@ const Header = () => {
             BuddyChat
           </span>
         </div>
+        </Link>
 
         <nav>
           {isMobile && (
@@ -47,8 +56,8 @@ const Header = () => {
             className={cn("tw:flex tw:gap-3",
             //Mobile
               {
-                "tw:hidden tw:flex-col tw:absolute tw:inset-x-0 tw:c-bg-theme-strong tw:top-full":isMobile,
-                "tw:flex": modalToggle
+                "tw:overflow-hidden tw:flex-col tw:absolute tw:inset-x-0 tw:c-bg-theme-strong tw:top-full tw:duration-200 tw:transition-[max-height] tw:max-h-0":isMobile,
+                "tw:flex tw:max-h-screen": modalToggle
               },           
             //Tablet & Desktop
               {"tw:grow tw:justify-end": !isMobile },
