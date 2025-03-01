2f24fd649d9002300e9c0063dfc09776b3604493
junik <wkdwnsdlr37@gmail.com>
2025-03-01 16:27:56 +0900

    Style: Chat컴포넌트 로직분리

--- src/widgets/chat/ui/index.tsx
+++ src/widgets/chat/ui/index.tsx
@@ -1,28 +1,21 @@
+import { Message } from './Message';
+import { timeStamp } from '@/shared/lib/time-stamp';
+
 const Chat = () => {
+  const _tempDate = new Date();
+
     return (
       <div className="tw:h-full tw:p-4 tw:flex tw:flex-col tw:gap-4">
         <div className="tw:overflow-y-scroll tw:grow tw:text-service-gray tw:flex tw:flex-col tw:gap-2">
-          <div className="tw:p-2 tw:border-1 tw:border-service-secondary tw:rounded-lg tw:text-center">
+          <div className="card">
             <p>익명의 상대와 1 : 1 대화를 시작합니다</p>
-            <p>2025-01-01 18:00</p>
+            <p>{timeStamp(_tempDate)}</p>
           </div>
-          <div>
-            <span>상대방</span>
-            <div className="tw:flex tw:justify-start tw:items-center tw:gap-1">
-              <span className="tw:bg-service-secondary tw:text-white tw:p-2 tw:rounded-lg">
-                안녕하세요
-              </span>
-              <span>16:01</span>
-            </div>
-          </div>
-          <div>
-            <div className="tw:flex tw:justify-end tw:items-center tw:gap-1">
-              <span>16:01</span>
-              <span className="tw:bg-service-secondary tw:text-white tw:p-2 tw:rounded-lg">
-                안녕하세요
-              </span>
-            </div>
-          </div>
+
+          <Message time={timeStamp(_tempDate, 'HH-mm')}>안녕하세요</Message>
+          <Message time={timeStamp(_tempDate, 'HH-mm')} isMine={true}>안녕하세요</Message>
+
+
         </div>
         <div className="tw:mt-5 tw:p-2 tw:border-1 tw:border-service-secondary tw:rounded-lg tw:flex tw:gap-2 tw:relative">
           <p className="tw:absolute tw:-top-1/2 tw:left-0 tw:right-0 tw:text-service-gray">
