2c588a9947912fea25c69a68d01ae215533443eb
junik <wkdwnsdlr37@gmail.com>
2025-03-02 13:00:07 +0900

    Style: MenuList css 변경

--- src/widgets/header/ui/MenuList.tsx
+++ src/widgets/header/ui/MenuList.tsx
@@ -18,28 +18,28 @@ export const MenuList = () => {
       2. 테마변경 버튼 디자인 변경예정
     */}
       <Link to="/" className="tw:flex tw:justify-center tw:items-center tw:p-3 tw:gap-3 tw:group tw:cursor-pointer">
-        <Home className="tw:w-5 tw:h-5 tw:text-balck tw:fill-service-gray tw:group-hover:fill-service-secondary" />
+        <Home className="tw:w-5 tw:h-5 tw:fill-service-gray tw:group-hover:fill-service-secondary" />
         <span className="tw:min-w-15 tw:text-center tw:group-hover:text-service-secondary tw:c-text-theme-base tw:text-base tw:font-medium">
           소개
         </span>
       </Link>
 
       <Link to={`/${path.random}`} className="tw:flex tw:justify-center tw:gap-3 tw:p-3 tw:group tw:cursor-pointer">
-        <Random className="tw:w-5 tw:h-5 tw:text-balck tw:fill-service-gray tw:group-hover:fill-service-secondary" />
+        <Random className="tw:w-5 tw:h-5 tw:fill-service-gray tw:group-hover:fill-service-secondary" />
         <span className="tw:min-w-15 tw:text-center tw:group-hover:text-service-secondary tw:c-text-theme-base tw:text-base tw:font-medium">
           랜덤채팅
         </span>
       </Link>
 
       <Link to={`/${path.group}`} className="tw:flex tw:justify-center tw:gap-3 tw:p-3 tw:group tw:cursor-pointer">
-        <Group className="tw:w-5 tw:h-5 tw:text-balck tw:fill-service-gray tw:group-hover:fill-service-secondary" />
+        <Group className="tw:w-5 tw:h-5 tw:fill-service-gray tw:group-hover:fill-service-secondary" />
         <span className="tw:min-w-15 tw:text-center tw:group-hover:text-service-secondary tw:c-text-theme-base tw:text-base tw:font-medium">
           그룹채팅
         </span>
       </Link>
 
       <button className="tw:flex tw:justify-center tw:gap-3 tw:p-3 tw:group tw:cursor-pointer" onClick={toggleTheme}>
-        <Theme className="tw:w-5 tw:h-5 tw:text-balck tw:fill-service-gray tw:group-hover:fill-service-secondary" />
+        <Theme className="tw:w-5 tw:h-5 tw:fill-service-gray tw:group-hover:fill-service-secondary" />
         <span className="tw:min-w-15 tw:text-center tw:group-hover:text-service-secondary tw:c-text-theme-base tw:text-base tw:font-medium">
           테마변경
         </span>
