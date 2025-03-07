ffbd5bcc75c038d9e3f7ac9e1c2710e4b7202c20
junik <wkdwnsdlr37@gmail.com>
2025-03-07 19:03:32 +0900

    Refactor: timeStamp 함수 조건추가

--- src/shared/lib/time-stamp.ts
+++ src/shared/lib/time-stamp.ts
@@ -2,8 +2,12 @@ const timeStamp = (date: Date, form?: FormType) => {
   const years = date.getFullYear();
   const months = (date.getMonth() + 1).toString().padStart(2, "0");
   const day = date.getDate().toString().padStart(2, "0");
-  const hours = date.getHours().toString().padStart(2, "0");
   const minutes = date.getMinutes().toString().padStart(2, "0");
+
+  let hours = date.getHours();
+  hours = hours % 12 || 12;
+  const period = hours < 12 ? "오후" : "오전";
+  const changeHours = hours.toString().padStart(2, "0");
 
   switch (form) {
     case "YY-MM":
@@ -11,9 +15,9 @@ const timeStamp = (date: Date, form?: FormType) => {
     case "YY-MM-DD":
       return `${years}-${months}-${day}`;
     case "HH-mm":
-      return `${hours}:${minutes}`;
+      return `${period} ${changeHours}:${minutes}`;
     default:
-      return `${years}-${months}-${day} ${hours}:${minutes}`;
+      return `${years}-${months}-${day} ${period} ${changeHours}:${minutes}`;
   }
 };
 
