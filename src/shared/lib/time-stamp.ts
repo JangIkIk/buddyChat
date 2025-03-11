bef69aa7ed7d6bfa068a569b31914fffe4f6ea90
junik <wkdwnsdlr37@gmail.com>
2025-03-11 18:52:43 +0900

    Refactor: timeStamp 함수 Date형식 맵핑

--- src/shared/lib/time-stamp.ts
+++ src/shared/lib/time-stamp.ts
@@ -1,20 +1,21 @@
-const timeStamp = (date: Date, form?: FormType) => {
-  const years = date.getFullYear();
-  const months = (date.getMonth() + 1).toString().padStart(2, "0");
-  const day = date.getDate().toString().padStart(2, "0");
-  const minutes = date.getMinutes().toString().padStart(2, "0");
+const timeStamp = (date: string, form?: FormType) => {
+  const dateWrap = new Date(date);
+  const years = dateWrap.getFullYear();
+  const months = (dateWrap.getMonth() + 1).toString().padStart(2, "0");
+  const day = dateWrap.getDate().toString().padStart(2, "0");
+  const minutes = dateWrap.getMinutes().toString().padStart(2, "0");
 
-  let hours = date.getHours();
+  let hours = dateWrap.getHours();
   hours = hours % 12 || 12;
   const period = hours < 12 ? "오후" : "오전";
   const changeHours = hours.toString().padStart(2, "0");
 
   switch (form) {
-    case "YY-MM":
+    case "YYYY-MM":
       return `${years}-${months}`;
-    case "YY-MM-DD":
+    case "YYYY-MM-DD":
       return `${years}-${months}-${day}`;
-    case "HH-mm":
+    case "HH:mm":
       return `${period} ${changeHours}:${minutes}`;
     default:
       return `${years}-${months}-${day} ${period} ${changeHours}:${minutes}`;
@@ -23,4 +24,4 @@ const timeStamp = (date: Date, form?: FormType) => {
 
 export { timeStamp };
 
-type FormType = "YY-MM" | "YY-MM-DD" | "HH-mm";
+type FormType = "YYYY-MM" | "YYYY-MM-DD" | "HH:mm";
