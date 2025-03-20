1cc0e014d88db63cf66dcea36bf42d421a2ece31
junik <wkdwnsdlr37@gmail.com>
2025-03-20 18:56:11 +0900

    Refactor: messageSocket 분리

--- src/entities/use-chat-message-socket.ts
+++ src/entities/use-chat-message-socket.ts
@@ -1,16 +1,16 @@
 import { useSocketConnection } from "@/shared/store/use-socket-connection";
 
-const useChatSendMessage = () => {
+const useChatMessageSocket = () => {
     const { socket } = useSocketConnection();
 
-    const submitMessage = ( message: string, callback: (status:number) => void ) => {
+    const sendChatMessage = ( message: string, callback: (status:number) => void ) => {
         if (!socket) return;
         socket.emit("chat-message", message, (res: {status: number}) => {
             callback(res.status)
         })
       }
-      return { submitMessage };
+      return { sendChatMessage };
     
 };
 
-export { useChatSendMessage };
+export { useChatMessageSocket };
