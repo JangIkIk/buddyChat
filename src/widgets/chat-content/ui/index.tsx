b5e764c15ce14fa91f69b23d00ca4b0516f382a9
junik <wkdwnsdlr37@gmail.com>
2025-03-22 16:23:06 +0900

    Feat: chatContent 생성

---
+++ src/widgets/chat-content/ui/index.tsx
@@ -0,0 +1,31 @@
+import ChatMessage  from "./chat-message";
+import { useChatMessageList } from '../model/use-chat-message-list';
+import { timeStamp } from "@/shared/lib/time-stamp";
+
+
+type ChatContentProps = {
+  matchTime?: string;
+  matchStartAlert: string;
+}
+
+const ChatContent = (props: ChatContentProps) => {
+  const { matchTime, matchStartAlert } = props;
+  const { messageList } = useChatMessageList();
+
+  return (
+    <div className="tw:h-full tw:flex tw:flex-col tw:gap-5 tw:c-bg-theme-strong tw:overflow-y-scroll">
+        {matchTime && matchStartAlert && ( // 입장알람
+          <div className="card tw:text-center tw:text-service-gray">
+            <p>{matchStartAlert}</p>
+            {matchTime && <p>{timeStamp(matchTime)}</p>}
+          </div>
+        )}
+        
+        {messageList.map((item, idx) => { // 채팅리스트
+          return <ChatMessage key={idx} {...item} />;
+        })}
+    </div>
+  );
+};
+
+export default ChatContent;
