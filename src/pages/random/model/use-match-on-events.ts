5675e13a0b22002f9e9391e241f2946ad1b63c2a
junik <wkdwnsdlr37@gmail.com>
2025-03-29 13:23:54 +0900

    Refactor: matchOnevents receiveChatMessage 추가

--- src/pages/random/model/use-match-on-events.ts
+++ src/pages/random/model/use-match-on-events.ts
@@ -1,33 +1,48 @@
+// package
 import { useEffect, useState } from "react";
+// layer
 import { randomMatch } from "@/entities/socket-random-match";
 import { useSocketConnection } from "@/shared/store/use-socket-connection";
 import { roomAlert } from '@/entities/socket-room-alert';
+import { chatMessage } from "@/entities/socket-chat-message";
 import { useMergeList } from '@/widgets/chat-content'
 
-const useMatchOnEvents = () => {
-  // Socket
-  const { socket } = useSocketConnection();
+type Match = boolean | null;
 
-  // Match State
-  const [match, setMatch] = useState<boolean | null>(null);
+interface MatchOnEventsReturn {
+  match: Match,
+  waiting: boolean,
+  startMatch: () => void,
+  cancelMatch: () => void,
+
+};
+
+/**
+ * @fileoverview
+ * - 서버 수신 핸들러 (매치시간 초과, 룸 알람, 채팅 메세지)
+ * - 서버 송신 핸들러 (매치시작, 매치취소)
+ */
+
+const useMatchOnEvents = (): MatchOnEventsReturn => {
+
+  const [match, setMatch] = useState<Match>(null);
   const [waiting, setWaiting] = useState<boolean>(false);
 
-  // Match API
-  const { connectMatch, disconnectMatch, resultMatch, removeListener: randomMatchRemove } = randomMatch(socket); 
+  const { socket } = useSocketConnection();
+  const mergeAction = useMergeList( state => state.action)
+
+  const { connectMatch, disconnectMatch, receiveMatchTimeout, removeListener: randomMatchRemove } = randomMatch(socket); 
   const { receiveRoomAlert, removeListener: roomAlertRemove } = roomAlert(socket);
+  const { receiveChatMessage, removeListener } = chatMessage(socket);
 
-  // MergeList
-  const saveAlert = useMergeList( state => state.action.saveAlert);
-
-  // MatchStart
-  const startHandler = () => {
+  
+  const startMatch = () => {
     connectMatch((res) => {
       if (res.status === 201) setWaiting(true);
     });
   };
 
-  // MatchCancel
-  const cancelHandler = () => {
+  const cancelMatch = () => {
     setMatch(null);
     disconnectMatch((res) => {
       if (res.status === 204) setWaiting(false);
@@ -36,11 +51,8 @@ const useMatchOnEvents = () => {
 
   useEffect(() => {
     if (!socket) return;
-    // if socket is connected, start the match
-    startHandler();
-
-    // timeout API
-    resultMatch((res) => {
+    startMatch();
+    receiveMatchTimeout((res) => {
       switch (res.status) {
         case 408:
           setMatch(false);
@@ -49,34 +61,49 @@ const useMatchOnEvents = () => {
         default:
           setMatch(null);
           setWaiting(false);
-          console.warn("cannot find the status code");
+          console.warn("cannot find the receiveMatchResult status code");
       }
     });
-
-    // if match successfull, saveAlert
+    
     receiveRoomAlert((res)=>{
       switch(res.status){
         case 200:
-          saveAlert(res.data)
+          mergeAction.saveAlert(res.data)
           setMatch(true);
           break;
+        case 204:
+          mergeAction.saveAlert(res.data)
+          break;
+        case 206:
+          break;
         default:
-          alert("채팅방 입장에 실패했습니다.");
-          console.warn("cannot find the status code");
+          console.warn("cannot find the reciveRoomAlert status code");
       }
     })
+
+    receiveChatMessage((res) => {
+      const { status, data } = res;
+      switch (status) {
+        case 201:
+          mergeAction.saveChatMessage(data);
+          break;
+        default:
+          console.warn("cannot find the receiveChatMessage status code");
+      }
+  });
 
     return () => {
       randomMatchRemove();
       roomAlertRemove();
+      removeListener();
     }
   }, [socket]);
 
   return {
     match,
     waiting,
-    startHandler,
-    cancelHandler,
+    startMatch,
+    cancelMatch,
   };
 };
 
