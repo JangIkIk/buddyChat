5308b790b6e62bdf8f09a7849853574f77858ed0
junik <wkdwnsdlr37@gmail.com>
2025-03-30 18:50:10 +0900

    Refactor: random-match type 추가

--- src/entities/socket-random-match.ts
+++ src/entities/socket-random-match.ts
@@ -1,35 +1,62 @@
-import { type BaseResponse, type BaseCallback} from './types';
+import {
+  type BaseResponse,
+  type BaseCallback,
+  type RemoveListener,
+  type EmptyCallback,
+} from "./types";
 
-const randomMatch = ( socket: GlobalSocket ) => {
-  const emptyCallback = () => console.warn("Socket not connected");
-  
-  if(!socket){
-    return { connectMatch: emptyCallback, disconnectMatch: emptyCallback, resultMatch: emptyCallback, removeListener: emptyCallback};
+type ConnectMatch = (callback: BaseCallback) => void;
+type DisconnectMatch = (callback: BaseCallback) => void;
+type ReceiveMatchTimeout = (callback: BaseCallback) => void;
+type RandomMatchReturn = {
+  connectMatch: ConnectMatch;
+  disconnectMatch: DisconnectMatch;
+  receiveMatchTimeout: ReceiveMatchTimeout;
+  removeListener: RemoveListener;
+};
+
+/**
+ * @FileDesc
+ * - 서버 수신 이벤트 ( 매치시간 초과 )
+ * - 서버 송신 이벤트 ( 매치시작, 매치취소 )
+ * - 수신이벤트 해제
+ */
+const randomMatch = (socket: GlobalSocket): RandomMatchReturn => {
+  const emptyCallback: EmptyCallback = () =>
+    console.warn("Socket not connected");
+
+  if (!socket) {
+    return {
+      connectMatch: emptyCallback,
+      disconnectMatch: emptyCallback,
+      receiveMatchTimeout: emptyCallback,
+      removeListener: emptyCallback,
+    };
   }
 
-  const connectMatch = (callback: BaseCallback) => {
+  const connectMatch: ConnectMatch = (callback) => {
     socket.emit("match-start", (res: BaseResponse) => {
-        callback(res);
+      callback(res);
     });
   };
 
-  const disconnectMatch = (callback: BaseCallback) => {
+  const disconnectMatch: DisconnectMatch = (callback) => {
     socket.emit("match-cancel", (res: BaseResponse) => {
       callback(res);
     });
   };
 
-  const resultMatch = (callback: BaseCallback) => {
+  const receiveMatchTimeout: ReceiveMatchTimeout = (callback) => {
     socket.on("match-result", (res: BaseResponse) => {
       callback(res);
     });
   };
 
-  const removeListener = () => {
-    socket.off("match-result", resultMatch);
-  }
+  const removeListener: RemoveListener = () => {
+    socket.off("match-result", receiveMatchTimeout);
+  };
 
-  return { connectMatch, disconnectMatch, resultMatch, removeListener };
+  return { connectMatch, disconnectMatch, receiveMatchTimeout, removeListener };
 };
 
 export { randomMatch };
