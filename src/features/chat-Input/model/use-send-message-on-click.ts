383f2a9e5d2d3b728da4e61a747cb9cd59b011c5
junik <wkdwnsdlr37@gmail.com>
2025-03-30 18:51:46 +0900

    Refactor: sned-message-on-click 설명 및 type추가

--- src/features/chat-Input/model/use-send-message-on-click.ts
+++ src/features/chat-Input/model/use-send-message-on-click.ts
@@ -1,17 +1,27 @@
-import { RefObject } from "react";
+// type
+import { type RefObject } from "react";
+// layer
 import { chatMessage } from "@/entities/socket-chat-message";
 import { chatMessageRegex } from "@/shared/consts/regex";
 import { useSocketConnection } from '@/shared/store/use-socket-connection';
 
+type SendMessageOnClickReturn = {
+  clickHandler: () => void,
+};
+
+/**
+ * @FileDesc
+ * - 서버 송신 핸들러 (채팅 메세지)
+ * -
+*/
 const useSendMessageOnClick = (
   chatRef: RefObject<HTMLTextAreaElement | null>
-) => {
+): SendMessageOnClickReturn => {
   const { socket } = useSocketConnection();
   const { sendChatMessage } = chatMessage(socket);
 
   const clickHandler = () => {
     if (!chatRef.current) return;
-    if (chatRef.current.value === "KEYWORD") return console.log("키워드전송");
 
     if (chatMessageRegex(chatRef.current.value)) {
       sendChatMessage(chatRef.current.value, (res) => {
@@ -21,7 +31,7 @@ const useSendMessageOnClick = (
             break;
           default:
             alert("메세지를 전송할수가 없습니다.");
-            console.warn("cannot find the status code");
+            console.warn("cannot find the sendChatMessage(Mouse) status code");
         }
       });
     }
