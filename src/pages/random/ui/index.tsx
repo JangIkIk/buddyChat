c7e4df8857136c088c83ff16bd370bbd5c697c5d
junik <wkdwnsdlr37@gmail.com>
2025-03-30 18:55:17 +0900

    Refactor: randomPage 설명 및 type 추가

--- src/pages/random/ui/index.tsx
+++ src/pages/random/ui/index.tsx
@@ -1,71 +1,85 @@
+// type
+import { type ReactElement } from 'react';
+// slice
 import { useMatchOnEvents } from "../model/use-match-on-events";
+// layer
+import ChatContent from "@/widgets/chat-content";
+import { ChatInput } from "@/features/chat-Input";
+import { ChatOutButton } from "@/features/chat-out-button";
 import { Button } from "@/shared/ui/Button";
 import { Loading } from "@/shared/ui/Loading";
-import ChatContent from "@/widgets/chat-content";
-import { ChatInput } from '@/features/chat-Input';
-import { ChatOutButton } from '@/features/chat-out-button';
 import { useSocketConnection } from "@/shared/store/use-socket-connection";
+import { useMergeList } from "@/shared/store/use-merge-list";
 
-const Random = () => {
+/**
+ * @fileoverview
+ * - 1:1 랜덤페이지 UI
+ * - 매치전, 매치중, 매치실패 UI
+ * - 매치완료 UI
+ */
+
+const Random = ():ReactElement => {
+
+  const { match, waiting, startMatch, cancelMatch } = useMatchOnEvents();
+  
   const { socketAction } = useSocketConnection();
-  const connectSocket = () => socketAction.connect("/random"); // local
-  // const connectSocket = () => socketAction.connect(""); // server
-  const { match, waiting, startHandler, cancelHandler } = useMatchOnEvents();
+  const connectSocket = () => socketAction.connect("/random");
+  const isRoom = useMergeList((state) => state.isRoom);
 
   return (
     <div className="tw:h-full tw:text-base tw:c-text-theme-base">
+
       {match ? (
         <div className="tw:h-full tw:flex tw:flex-col tw:p-4 tw:gap-1">
-          <ChatContent/>
-          <div className="tw:flex tw:gap-3 tw:items-center">
-          <ChatInput/>
-          <ChatOutButton/>
-          </div>
+          <ChatContent />
+          {!isRoom ? (
+            <div className="tw:flex tw:gap-3 tw:items-center">
+              <ChatInput />
+              <ChatOutButton />
+            </div>
+          ): <div>다시찾기 버튼</div>}
         </div>
       ) : (
         <div className="tw:h-full tw:c-linear-base tw:flex tw:flex-col tw:justify-center tw:items-center tw:gap-3">
-          {/* 매칭전 */}
-          {!waiting && match === null && (
+          
+          {!waiting && match === null && ( // 매치전
             <>
               <h1 className="tw:text-xl">
                 익명의 상대와 1 : 1 채팅을 시작하세요
               </h1>
-              <Button
-                intent={"select"}
-                onClick={connectSocket}
-              >
+              <Button intent={"select"} onClick={connectSocket}>
                 매칭 시작하기
               </Button>
             </>
           )}
-          {/* 대기중 */}
-          {waiting && (
+          
+          {waiting && ( // 매치중
             <>
               <h1 className="tw:text-xl tw:flex tw:flex-col tw:justify-center tw:items-center tw:gap-3">
                 <div className="tw:flex tw:items-center tw:gap-3">
                   <span>상대를 찾는중입니다..</span>
                   <Loading size="tw:size-8" />
                 </div>
-                <Button
-                  intent={"cancel"}
-                  onClick={cancelHandler}
-                >
+                <Button intent={"cancel"} onClick={cancelMatch}>
                   매칭 취소
                 </Button>
               </h1>
             </>
           )}
-          {/* 사용자를 찾을수 없을시 */}
-          { !waiting && match === false && (
+
+          {!waiting && match === false && ( // 매치실패
             <>
               <h1 className="tw:text-xl">
                 현재 매칭할 수 있는 사용자가 없습니다.
               </h1>
-              <Button intent={"select"} onClick={startHandler}>다시찾기</Button>
+              <Button intent={"select"} onClick={startMatch}>
+                다시찾기
+              </Button>
             </>
           )}
         </div>
       )}
+
     </div>
   );
 };
