5eaaf7046b389418bf5dbad519f216f7827e2e44
junik <wkdwnsdlr37@gmail.com>
2025-03-30 18:50:47 +0900

    Refactor: room-leave type 추가

--- src/entities/socket-room-leave.ts
+++ src/entities/socket-room-leave.ts
@@ -1,16 +1,23 @@
-import { type BaseResponse, type BaseCallback} from './types';
+import { type EmptyCallback } from './types';
 
-const roomLeave = ( socket: GlobalSocket ) => {
-    const emptyCallback = () => console.warn("Socket not connected");
+type RoomLeaveReturn = {
+    sendRoomLeave: EmptyCallback,
+}
+
+/**
+ * @FileDesc
+ * - 서버 수신 이벤트 (없음)
+ * - 서버 송신 이벤트 (방 나가기)
+*/
+const roomLeave = ( socket: GlobalSocket ): RoomLeaveReturn => {
+    const emptyCallback:EmptyCallback = () => console.warn("Socket not connected");
 
     if(!socket){
         return { sendRoomLeave: emptyCallback };
     }
 
-    const sendRoomLeave = ( callback: BaseCallback ) => {
-        socket.emit("room-outside", (res: BaseResponse) => {
-            callback(res);
-        });
+    const sendRoomLeave = () => {
+        socket.emit("room-outside");
     };
 
     return { sendRoomLeave };
