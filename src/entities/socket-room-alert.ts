d9191a5b1e468391c7466da9a64354f70d854edd
junik <wkdwnsdlr37@gmail.com>
2025-03-30 18:50:29 +0900

    Refactor: room-alert type 추가

--- src/entities/socket-room-alert.ts
+++ src/entities/socket-room-alert.ts
@@ -1,34 +1,78 @@
-import { type DataCallback, type DataResponse } from './types';
+import { type DataCallback, type DataResponse, type RemoveListener, type EmptyCallback } from './types';
 
 type JoinAlert = {
     joinTime: string;
     nickName: string | null;
     roomName: string | null;
     type: "join";   
-}
+};
 
 type OutAlert = {
     outTime: string;
     chatTime: string;
     nickName: string | null;
-    type: 'out'
+    socketId: string;
+    type: 'out';
+};
+
+class JoinAlertMapper {
+    joinTime: JoinAlert["joinTime"];
+    nickName: JoinAlert["nickName"];
+    roomName: JoinAlert["roomName"];
+    type: JoinAlert["type"];
+
+    constructor( data: JoinAlert){
+        this.joinTime = data.joinTime;
+        this.nickName = data.nickName;
+        this.roomName = data.roomName;
+        this.type = data.type;
+    }
+
+};
+
+class OutAlertMapper {
+    outTime: OutAlert["outTime"];
+    chatTime: OutAlert["chatTime"];
+    nickName: OutAlert["nickName"];
+    socketId: OutAlert["socketId"];
+    type: OutAlert["type"];
+
+    constructor( data: OutAlert){
+        this.outTime = data.outTime;
+        this.chatTime = data.chatTime;
+        this.nickName = data.nickName;
+        this.socketId = data.socketId;
+        this.type = data.type;
+    }
+
+};
+
+type ReceiveRoomAlert = ( callback: DataCallback<JoinAlertMapper | OutAlertMapper> ) => void;
+type RoomAlertReturn = {
+    receiveRoomAlert: ReceiveRoomAlert
+    removeListener: RemoveListener;
 }
 
-
-const roomAlert = ( socket: GlobalSocket ) => {
-    const emptyCallback = () => console.warn("Socket not connected");
+/**
+ * @FileDesc
+ * - 서버 수신 이벤트 (입장알람, 퇴장알람)
+ * - 서버 송신 이벤트 (없음)
+*/
+const roomAlert = ( socket: GlobalSocket ):RoomAlertReturn => {
+    const emptyCallback:EmptyCallback = () => console.warn("Socket not connected");
 
     if(!socket){
         return { receiveRoomAlert: emptyCallback, removeListener: emptyCallback };
     };
 
-    const receiveRoomAlert = ( callback: DataCallback<JoinAlert | OutAlert> ) => {
+    const receiveRoomAlert:ReceiveRoomAlert = ( callback ) => {
         socket.on("room-alert", ( res: DataResponse<JoinAlert | OutAlert> ) => {
-            callback(res);
+            const mapperData = res.data.type === "join" ? new JoinAlertMapper(res.data) : new OutAlertMapper(res.data);
+            callback({...res, data: mapperData})
         })
     };
 
-    const removeListener = () => {
+    const removeListener:RemoveListener = () => {
         socket.off("room-alert", receiveRoomAlert);
     };
 
