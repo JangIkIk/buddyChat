4e190ebaaa99ea13231cc558b147db0067909cc2
junik <wkdwnsdlr37@gmail.com>
2025-03-31 23:07:24 +0900

    Refactor: match 이벤트 조건추가

--- src/pages/random/model/use-match-on-events.ts
+++ src/pages/random/model/use-match-on-events.ts
@@ -28,7 +28,8 @@ const useMatchOnEvents = (): MatchOnEventsReturn => {
   const [waiting, setWaiting] = useState<boolean>(false);
 
   const { socket } = useSocketConnection();
-  const mergeAction = useMergeList( state => state.action)
+  const mergeAction = useMergeList( state => state.action);
+  const isRoom = useMergeList((state) => state.isRoom);
 
   const { connectMatch, disconnectMatch, receiveMatchTimeout, removeListener: randomMatchRemove } = randomMatch(socket); 
   const { receiveRoomAlert, removeListener: roomAlertRemove } = roomAlert(socket);
@@ -37,7 +38,10 @@ const useMatchOnEvents = (): MatchOnEventsReturn => {
   
   const startMatch = () => {
     connectMatch((res) => {
-      if (res.status === 201) setWaiting(true);
+      if (res.status === 201) {
+        setWaiting(true); 
+        setMatch(false);
+      }
     });
   };
 
@@ -50,47 +54,53 @@ const useMatchOnEvents = (): MatchOnEventsReturn => {
 
   useEffect(() => {
     if (!socket) return;
+    // 페이지 이동시에 socket이 안끊어지므로 랜덤채팅페이지 다시넘어오면 매치가 자동으로 발생
+    // socket끊는것을 고민해보자
     startMatch();
+
     receiveMatchTimeout((res) => {
-      switch (res.status) {
-        case 408:
+      const { data } = res;
+      switch(data.type){
+        case "timeout":
           setMatch(false);
           setWaiting(false);
           break;
         default:
-          setMatch(null);
-          setWaiting(false);
-          console.warn("cannot find the receiveMatchResult status code");
+          console.warn("cannot find the receiveMatchTimeout type");
       }
     });
     
     receiveRoomAlert((res)=>{
-      switch(res.status){
-        case 200:
-          mergeAction.saveAlert(res.data)
+      const { data } = res;
+      switch(data.type){
+        case "join":
+          if(!isRoom) mergeAction.reSet();
+          mergeAction.checkRoom();
+          mergeAction.saveAlert(data);
           setMatch(true);
           break;
-        case 204:
-          mergeAction.saveAlert(res.data);
+        case "out":
+          mergeAction.saveAlert(data);
           mergeAction.checkRoom();
           break;
-        case 206:
+        case "midnight":
+          // 자정 시간 형식 대기
           break;
         default:
-          console.warn("cannot find the reciveRoomAlert status code");
+          console.warn("cannot find the reciveRoomAlert type");
       }
-    })
+    });
 
     receiveChatMessage((res) => {
-      const { status, data } = res;
-      switch (status) {
-        case 201:
+      const { data } = res;
+      switch (data.type) {
+        case "message":
           mergeAction.saveChatMessage(data);
           break;
         default:
-          console.warn("cannot find the receiveChatMessage status code");
-      }
-  });
+          console.warn("cannot find the receiveChatMessage type");
+        }
+    });
 
     return () => {
       randomMatchRemove();
